e4a8c6fc04c6f16783fa917fea6a6e320b79a532
DongManhDung <dongdung0407@gmail.com>
2025-03-04 16:26:26 +0700

    feat: Working on Design Pattern Project, not use Design Pattern

--- designPatternExample/src/main/java/com/fit/Main.java
+++ designPatternExample/src/main/java/com/fit/Main.java
@@ -1,7 +1,14 @@
 package com.fit;
 
+import com.fit.enties.Employee;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1. Không dùng Design Pattern");
+        Employee e1 = new Employee("Nguyễn Văn A", "Đội trưởng");
+        e1.performDuties();
+
+        
+
     }
 }
---
+++ designPatternExample/src/main/java/com/fit/enties/Employee.java
@@ -0,0 +1,27 @@
+package com.fit.enties;
+
+public class Employee {
+    String name;
+    String role;
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public void performDuties() {
+        switch (role) {
+            case "Đội trưởng":
+                System.out.println(name + " đi tuần và gán việc nhân viên.");
+                break;
+            case "Giám đốc":
+                System.out.println(name + " quản lý công ty.");
+                break;
+            case "Nhân viên VP":
+                System.out.println(name + " pha và phê");
+                break;
+            default:
+                System.out.println(name + " không có nhiệm vụ cụ thể");
+        }
+    }
+}
